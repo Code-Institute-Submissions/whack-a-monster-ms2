b46c7abe1e9c8e28491856be97b95aebf860434d
dovi-k <dovylee@gmail.com>
2020-09-22 19:52:08 +0000

    added startGame function to script.js

--- script.js
+++ script.js
@@ -11,5 +11,26 @@ let score = 0;
 let countdown;
 
 function pickRandomHole(holes){
-    const randomHole = Math.floor(Math.random()*holes.length)
+    const randomHole = Math.floor(Math.random()*holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole){
+        return pickRandomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function popOut(){
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add("up");
+    setTimeout(function(){
+        hole.classList.remove("up");
+        if (!timeUp) popOut();
+    }, time);
+}
+
+popOut();
+
+function startGame(){
+    countdown = 20;
 }
